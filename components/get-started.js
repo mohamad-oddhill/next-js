ba6d80fadb69516f2fab3cd10871925e4f55e2ca
mohamad-oddhill <mohamad.rashid@oddhill.se>
2024-11-05 00:22:16 +0100

    Force deploy

--- components/get-started.js
+++ components/get-started.js
@@ -9,7 +9,7 @@ export function GetStarted () {
             <div className={style.get__started}>
                 <Image src={getStarted} />
                 <div className={style.content}>
-                    <h2>Become a member today</h2>
+                    <h2>Become a member today!!</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                     <Link className={style.primary__btn} href={"/"}>Get started</Link>
                 </div>
